eeed22f8d9d0a1a6909e4f57f915f6a1e6b2817e
snipextt <snipextt@gmail.com>
2021-09-01 17:04:40 +0530

    Add box shadow to meeting controls

--- src/Pages/VirtualClassroom/VirtualClassroom.tsx
+++ src/Pages/VirtualClassroom/VirtualClassroom.tsx
@@ -39,6 +39,8 @@ const meetingControlsStyles: Partial<IStackItemStyles> = {
     borderRadius: 20,
     padding: 10,
     justifyContent: 'space-around',
+    boxShadow:
+      'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px',
   },
 };
 
